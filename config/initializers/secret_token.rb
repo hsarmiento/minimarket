8b05ce5db4375d024a638a213e6d3b4a97fe6189
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2012-12-31 11:46:33 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minimarket::Application.config.secret_token = '97dc6f60ab124a3cc7a1b96b2d0b44c5e0153e24718a3884523e50ed5e43051f2f74b8162eac375685f2915245f401be22fbd3456d2e10581353e0e2ad3ac311'
